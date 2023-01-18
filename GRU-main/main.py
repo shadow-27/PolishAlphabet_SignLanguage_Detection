792561d0d1c9906271ad98acd5ffe6f78c075890
Szymon <smagowski.szymon@gmail.com>
2023-01-18 20:15:51 +0100

    small bug fixed

--- GRU-main/main.py
+++ GRU-main/main.py
@@ -98,7 +98,7 @@ all_letters = ['a',
                 'z',
                 'ź',
                 'ż',
-                'none']
+                'non']
 
 boundry = 0.6
 
@@ -446,7 +446,7 @@ def show_frames(cap, label, wordText, suggestedWordText, sentence,obj):
     global mode, button_dict
 
     recognized_letter_left = ""
-    recognized_letter_right =" "
+    recognized_letter_right = ""
     suggestedWords = []
     # print(obj.distance_left)
     fps = cvFpsCalc.get()
@@ -652,13 +652,11 @@ def show_frames(cap, label, wordText, suggestedWordText, sentence,obj):
                 all_letters[pred_left],
                 None,
             )
-            recognized_letter_left = all_letters[pred_left]
-            recognized_letter_right = all_letters[pred_right]
     else:
         point_history.append([0, 0])
     
     if((not hands_list.__contains__("Right")) and obj.number_of_frames_right > 1):
-        print("Zostały klatki prawe")
+        #print("Zostały klatki prawe")
         pred_right = predict_values_right(obj)
         if(pred_right!=38):
             obj.timer_right_hand = time.time()
@@ -675,7 +673,7 @@ def show_frames(cap, label, wordText, suggestedWordText, sentence,obj):
             obj.coordinates_with_movement_right = []
 
     if((not hands_list.__contains__("Left")) and obj.number_of_frames_left > 1):
-        print("Zostały klatki prawe")
+        #print("Zostały klatki prawe")
         pred_left = predict_values_left(obj)
         if(pred_left!=38):
             obj.timer_left_hand = time.time()
@@ -700,14 +698,21 @@ def show_frames(cap, label, wordText, suggestedWordText, sentence,obj):
     label.photoImage = photoImage
     label.configure(image=photoImage)
 
+    recognized_letter_left = all_letters[pred_left]
+    recognized_letter_right = all_letters[pred_right]
+
     # recognized_letter = all_letters[5]
     #print(recognized_letter)
 
     # print result from right hand
     if recognized_letter_right and pred_right!=38:
+        # print("Right:")
+        # print(pred_right)
+        # print(recognized_letter_right)
+
         readText = wordText.get("1.0", 'end-1c')
         readSentence = sentence.get("1.0", 'end-1c')
-
+        
         sign_length = len(recognized_letter_right)
         if not readText or recognized_letter_right.lower() != readText[-sign_length:]:
             readText += recognized_letter_right.lower()
@@ -746,6 +751,9 @@ def show_frames(cap, label, wordText, suggestedWordText, sentence,obj):
 
     # print result from left hand
     if recognized_letter_left and pred_left!=38:
+        # print("Left:")
+        # print(pred_left)
+        # print(recognized_letter_left)
         readText = wordText.get("1.0", 'end-1c')
         readSentence = sentence.get("1.0", 'end-1c')
 
@@ -797,136 +805,3 @@ def start_app(cap, label, root, wordText, suggestedWordText, sentence):
 
 r, c, l, wt, swt, s = create_window()
 start_app(c, l, r, wt, swt, s)
-
-# # Define function to show frame
-# def show_frames(cap, label, wordText, suggestedWordText, sentence):
-#     global mode, button_dict
-
-#     recognized_letter = ""
-#     suggestedWords = []
-
-#     fps = cvFpsCalc.get()
-
-#     # Process Key (ESC: end)
-#     key = cv2.waitKey(10)
-#     if key == 32:
-#         letter_approved = True
-#     # else:
-#     #     print(key)
-#     number, mode = select_mode(key, mode)
-
-#     # Get the latest frame and convert into Image
-#     ret, cv2image = cap.read()[0], cv2.cvtColor(cap.read()[1], cv2.COLOR_BGR2RGB)
-#     if not ret:
-#         return
-
-#     image = cv2.flip(cv2image, 1)
-#     debug_image = copy.deepcopy(image)
-
-#     image.flags.writeable = False
-#     results = hands.process(image)
-#     image.flags.writeable = True
-#     # Hand landmarks
-#     if results.multi_hand_landmarks is not None:
-#         for hand_landmarks, handedness in zip(results.multi_hand_landmarks,
-#                                               results.multi_handedness):
-#             # Bounding box calculation
-#             brect = calc_bounding_rect(debug_image, hand_landmarks)
-#             # Landmark calculation
-#             landmark_list = calc_landmark_list(debug_image, hand_landmarks)
-
-#             # Conversion to relative coordinates / normalized coordinates
-#             pre_processed_landmark_list = pre_process_landmark(
-#                 landmark_list)
-#             pre_processed_point_history_list = pre_process_point_history(
-#                 debug_image, point_history)
-
-#             print(handedness.classification[0].label)
-
-#             # Write to the dataset file
-#             logging_csv(number, mode, pre_processed_landmark_list,
-#                         pre_processed_point_history_list)
-
-#             # Hand sign classification
-#             pre_processed_landmark_list = np.expand_dims(pre_processed_landmark_list, axis=0)
-#             hand_sign_id = keypoint_classifier(pre_processed_landmark_list)
-#             if hand_sign_id == "Not applicable":  # Point gesture
-#                 point_history.append(landmark_list[8])
-#             else:
-#                 point_history.append([0, 0])
-
-#             # Finger gesture classification
-#             finger_gesture_id = 0
-#             point_history_len = len(pre_processed_point_history_list)
-#             if point_history_len == (history_length * 2):
-#                 finger_gesture_id = point_history_classifier(
-#                     pre_processed_point_history_list)
-
-#             # Calculates the gesture IDs in the latest detection
-#             finger_gesture_history.append(finger_gesture_id)
-#             most_common_fg_id = Counter(
-#                 finger_gesture_history).most_common()
-
-#             # Drawing part
-#             debug_image = draw_bounding_rect(use_brect, debug_image, brect)
-#             debug_image = draw_landmarks(debug_image, landmark_list)
-#             debug_image, recognized_letter = draw_info_text(
-#                 debug_image,
-#                 brect,
-#                 handedness,
-#                 keypoint_classifier_labels[hand_sign_id],
-#                 point_history_classifier_labels[most_common_fg_id[0][0]],
-#             )
-#     else:
-#         point_history.append([0, 0])
-
-#     debug_image = draw_point_history(debug_image, point_history)
-#     debug_image = draw_info(debug_image, fps, mode, number)
-#     debug_image = Image.fromarray(debug_image)
-
-#     # Convert image to PhotoImage
-#     photoImage = ImageTk.PhotoImage(image=debug_image)
-#     label.photoImage = photoImage
-#     label.configure(image=photoImage)
-
-#     if recognized_letter:
-#         readText = wordText.get("1.0", 'end-1c')
-#         readSentence = sentence.get("1.0", 'end-1c')
-
-#         if not readText or recognized_letter[-1].lower() != readText[-1]:
-#             readText += recognized_letter[-1].lower()
-#             readSentence += recognized_letter[-1].lower()
-
-#             wordText.configure(state='normal')
-#             wordText.delete('1.0', tk.END)
-#             wordText.insert('1.0', readText)
-#             wordText.tag_add(1.0, "end")
-#             wordText.configure(state='disabled')
-
-#             sentence.configure(state='normal')
-#             sentence.delete('1.0', tk.END)
-#             sentence.insert('1.0', readSentence)
-#             sentence.tag_add(1.0, "end")
-#             sentence.configure(state='disabled')
-
-#             if not h.spell(readText):
-#                 suggestedWords.append(h.suggest(readText))
-#             else:
-#                 suggestedWords.append(h.suffix_suggest(readText))
-#                 suggestedWords.append(h.stem(readText))
-
-#             for i in range(len(button_dict)):
-#                 button_dict[i].destroy()
-
-#             suggestedWords = [word for words in suggestedWords for word in words]
-#             suggestedWordsTrunc = suggestedWords[:len(suggestedWords) if len(
-#                 suggestedWords) < SUGGESTED_WORD_NUMBER else SUGGESTED_WORD_NUMBER]
-
-#             for i in range(len(suggestedWordsTrunc)):
-#                 button_dict[i] = tk.Button(suggestedWordText, text=suggestedWordsTrunc[i],
-#                                            command=lambda idx=i:
-#                                            wordCallback(button_dict[idx]['text'] + " ", wordText, sentence))
-#                 button_dict[i].pack(side=tk.RIGHT, padx=2, pady=1)
-
-#     # Repeat after an interval to capture continuously
-#     label.after(100, show_frames, cap, label, wordText, suggestedWordText, sentence)
