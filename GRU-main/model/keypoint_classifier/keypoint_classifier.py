b57e37cf15a2ee3b26f9eb80721bf59e9a1f98ba
Ayetijhya Desmukhya <ayeti0202@gmail.com>
2023-01-24 05:39:15 +0100

    Update keypoint_classifier.py

--- GRU-main/model/keypoint_classifier/keypoint_classifier.py
+++ GRU-main/model/keypoint_classifier/keypoint_classifier.py
@@ -6,22 +6,20 @@ import tensorflow as tf
 
 class KeyPointClassifier(object):
     def __init__(
-        self,
-        model_path='model/keypoint_classifier/GRU_model_moving.tflite',
-        num_threads=1,
+            self,
+            model_path='model/keypoint_classifier/GRU_model_moving.tflite',
+            num_threads=1,
     ):
         self.interpreter = tf.lite.Interpreter(model_path=model_path,
                                                num_threads=num_threads)
-
-        # print(self.interpreter.get_input_details())
 
         self.interpreter.allocate_tensors()
         self.input_details = self.interpreter.get_input_details()
         self.output_details = self.interpreter.get_output_details()
 
     def __call__(
-        self,
-        landmark_list,
+            self,
+            landmark_list,
     ):
         input_details_tensor_index = self.input_details[0]['index']
         self.interpreter.set_tensor(
@@ -37,11 +35,12 @@ class KeyPointClassifier(object):
 
         return np.squeeze(result)
 
+
 class KeyPointClassifierOneFrame(object):
     def __init__(
-        self,
-        model_path='model/keypoint_classifier/GRU_model_one_frame_aug.tflite',
-        num_threads=1,
+            self,
+            model_path='model/keypoint_classifier/GRU_model_one_frame_aug.tflite',
+            num_threads=1,
     ):
         self.interpreter = tf.lite.Interpreter(model_path=model_path,
                                                num_threads=num_threads)
@@ -53,8 +52,8 @@ class KeyPointClassifierOneFrame(object):
         self.output_details = self.interpreter.get_output_details()
 
     def __call__(
-        self,
-        landmark_list,
+            self,
+            landmark_list,
     ):
         input_details_tensor_index = self.input_details[0]['index']
         self.interpreter.set_tensor(
